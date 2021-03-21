9693deeb8d4deecf937c91a3e2b6ab888d002438
Germain Grumel <germaingrumel@outlook.fr>
2021-03-21 15:17:46 +0100

    added redux, context, DataLayer

--- src/DataLayer.js
+++ src/DataLayer.js
@@ -1,7 +1,9 @@
 import React, { createContext, useContext, useReducer } from "react";
 
-function DataLayer() {
-  return <div></div>;
-}
+export const DataLayerContext = createContext();
 
-export default DataLayer;
+export const DataLayer = ({ initialState, reducer, children }) => (
+  <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </DataLayerContext.Provider>
+);
